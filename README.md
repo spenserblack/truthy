906f1356f4050cce9f06c255b82ab3981ac812a6
Spenser Black <spenserblack01@gmail.com>
2020-08-05 16:28:14 -0400

    Add some docs

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # truthy
 Check if a value is "truthy"
+
+In other words,
+
+```rust
+// rust
+my_value.truthy();
+```
+
+Should behave similarly to
+
+```javascript
+// javascript
+!!myValue;
+Boolean(myValue);
+```
+or
+```python
+# python
+bool(my_value)
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+#![no_std]
+
+//! Mimic the "truthy" behavior of languages like JavaScript and Python.
+//!
+//! In other words,
+//!
+//! ```rust,ignore
+//! // rust
+//! my_value.truthy();
+//! ```
+//!
+//! Should behave similarly to
+//!
+//! ```javascript
+//! // javascript
+//! !!myValue;
+//! Boolean(myValue);
+//! ```
+//! or
+//! ```python
+//! # python
+//! bool(my_value)
+//! ```
+
+/// Convert to a `bool`.
+pub trait Truthy {
+    /// Converts `&self` to a `bool`.
+    fn truthy(&self) -> bool;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
